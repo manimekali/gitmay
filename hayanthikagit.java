82cffa7536383a06963fc1c85c548924d594534b
manimekalai <manimekalaij121998@gmail.com>
2024-05-07 12:44:04 +0530

    my task

---
+++ hayanthikagit.java
@@ -0,0 +1,32 @@
+package org.sample;
+
+public class hayanthikagit {
+	public void empname() {
+		System.out.println("employee name is:hayanthika");
+	}
+	public void empphno() {
+		System.out.println("employee phone number is:7448863673");
+	}
+	public void empaddress() {
+		System.out.println("employee address is:mugalivakkam");
+	}
+	public void empid() {
+		System.out.println("employee id is:117");
+	}
+	public void empdesignation() {
+		System.out.println("employee designetion is:software engineer");
+	}
+	public void empsalary() {
+		System.out.println("employee salary is:15lakh");
+	}
+	public static void main(String[]args) {
+		hayanthikagit h=new hayanthikagit();
+		h.empname();
+		h.empphno();
+		h.empaddress();
+		h.empid();
+		h.empdesignation();
+		h.empsalary();
+	}
+
+}
